a776cabf64b79a4d1b2a9a84903dc7c4100167b5
Nash.Lucas <Nash.Lucas@SEMS-259-16.SEMS259.LAB>
2020-02-12 16:26:38 -0800

    E

---
+++ src/_08_shiny_objects/Checkpoint.java
@@ -0,0 +1,59 @@
+package _08_shiny_objects;
+
+public class Checkpoint {
+public static void main(String[] args) {
+	for (int i = 1; i < 500 ; i+=2) {
+		
+		
+		
+System.out.println(i);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
+
